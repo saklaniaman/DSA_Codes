8264b3e72fde970b82c429f2106998fba96cde07
saklaniaman <89509591+saklaniaman@users.noreply.github.com>
2022-11-21 23:41:44 +0530

    Add files via upload

---
+++ toh.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void toh(int n, char from_rod, char to_rod, char aux_rod)
+{
+	if (n == 1)
+	{
+		printf("\n Move disk 1 from rod %c to rod %c", from_rod, to_rod);
+		return;
+	}
+	toh(n-1, from_rod, aux_rod, to_rod);
+	printf("\n Move disk %d from rod %c to rod %c", n, from_rod, to_rod);
+	toh(n-1, aux_rod, to_rod, from_rod);
+}
+
+int main()
+{
+	int n = 2; 
+	toh(n, 'S', 'H', 'D');
+	return 0;
+}
